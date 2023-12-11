5899f32dc71d3b9e3bdf26d537a66d80896479af
Yayunyun <110958060+Yayunyun@users.noreply.github.com>
2023-12-11 04:50:47 +0000

    chagne circle_transform, train and attack function

--- 10_code/utils.py
+++ 10_code/utils.py
@@ -173,10 +173,15 @@ class ToRange255(object):
 
 
 def init_patch_circle(image_size, patch_size):
+    """Initialize a random circle patch."""
     image_size = image_size**2
+    # Calculate the number of pixels in the circular patch
     noise_size = int(image_size * patch_size)
+    # Calculate the radius of the circular patch
     radius = int(math.sqrt(noise_size / math.pi))
+    # Initialize an empty patch with shape (1, 3, 2*radius, 2*radius)
     patch = np.zeros((1, 3, radius * 2, radius * 2))
+    # Loop through each color channel (R, G, B)
     for i in range(3):
         a = np.zeros((radius * 2, radius * 2))
         cx, cy = radius, radius  # The center of circle
@@ -198,11 +203,11 @@ def circle_transform(patch, data_shape, patch_shape, image_size, device="cuda"):
     # get shape
     m_size = patch_shape[-1]
 
-    for i in range(x.shape[0]):
+    for i in range(x.shape[0]): # number of samples in the batch i.e. Batch Size
         # random rotation
         rot = np.random.choice(360)
-        for j in range(patch[0][i].shape[0]):
-            patch[0][i] = rotate(patch[0][i], angle=rot, reshape=False)
+        for j in range(patch[0].shape[0]):
+            patch[0][j] = rotate(patch[0][j], angle=rot, reshape=False)
 
         # random location
         random_x = np.random.choice(image_size)
@@ -215,9 +220,9 @@ def circle_transform(patch, data_shape, patch_shape, image_size, device="cuda"):
                 random_y = np.random.choice(image_size)
 
         # apply patch to dummy image - question: am I applying this correctly?
-        x[i][
-            random_x : random_x + patch_shape[-1], random_y : random_y + patch_shape[-1]
-        ] = patch[0][i]
+        # x[i][
+        #     random_x : random_x + patch_shape[-1], random_y : random_y + patch_shape[-1]
+        # ] = patch[0][i] ## Elisa's code 
         # x[i][1][
         #    random_x : random_x + patch_shape[-1], random_y : random_y + patch_shape[-1]
         # ] = patch[0][i]
@@ -225,14 +230,20 @@ def circle_transform(patch, data_shape, patch_shape, image_size, device="cuda"):
         #    random_x : random_x + patch_shape[-1], random_y : random_y + patch_shape[-1]
         # ] = patch[0][2]
 
+        # apply patch to dummy image  - modification by yayun
+        x[i][0][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[0][0]
+        x[i][1][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[0][1]
+        x[i][2][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[0][2]
+
+
     mask = np.copy(x)
     mask[mask != 0] = 1.0
-
+    x_w_patch = x
     # x = torch.tensor(x).to(device)
     # mask = torch.tensor(mask).to(device)
     # patch = torch.tensor(patch).to(device)
 
-    return x, mask, patch.shape
+    return x_w_patch, mask, patch.shape
 
 
 def init_patch_square(image_size, patch_size):
@@ -296,15 +307,17 @@ def attack(
     max_count=1000,
 ):
     netClassifier.eval()
-
+    # x.shape  = (256, 3, 32, 32)
     x_out = F.softmax(netClassifier(x))
-    target_prob = x_out.data[0][target]  # QUESTION: why are we only taking one?
+    target_prob = x_out.data[0][target]# QUESTION: why are we only taking one? # i think i get why, but the reason is dumb i would say
+    print(target_prob) # for debugging
 
-    # extend mask to match dimensions of x
-    mask = mask.unsqueeze(0).expand(x.shape[0], -1, -1, -1)
+    # # extend mask to match dimensions of x
+    
+    # mask = mask.unsqueeze(0).expand(x.shape[0], -1, -1, -1)
 
     # extend patch to match dimensions of x
-    patch = patch.unsqueeze(0).expand(x.shape[0], -1, -1, -1)
+    # patch = patch.unsqueeze(0).expand(x.shape[0], -1, -1, -1)
 
     adv_x = torch.mul((1 - mask), x) + torch.mul(mask, patch)
 
@@ -368,7 +381,7 @@ def train(
         total += 1
 
         # transform path
-        data_shape = data[0].data.cpu().numpy().shape
+        data_shape = data.data.cpu().numpy().shape # keep it as original shape by yayun
 
         if patch_type == "circle":
             patch, mask, patch_shape = circle_transform(
